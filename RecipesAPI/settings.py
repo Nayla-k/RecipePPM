bc23cb462dffd2a521aac199a4dfb2569c495ecf
Nayla Kouider <nayla.kouider@stud.unifi.it>
2024-06-19 17:21:12 +0200

    second deploy config

--- RecipesAPI/settings.py
+++ RecipesAPI/settings.py
@@ -172,3 +172,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+  'https://recipesppm-production.up.railway.app'
+]
