630eeb02a7a4f93ba2dd2597dad0d66228b6660b
Nayla Kouider <nayla.kouider@stud.unifi.it>
2024-06-19 17:18:12 +0200

    deploy config

---
+++ recipes_app/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from .models import Recipe
+from djrichtextfield.widgets import RichTextWidget
+
+
+class RecipeForm(forms.ModelForm):
+    """Form to create a recipe"""
+
+    class Meta:
+        model = Recipe
+        fields = [
+            "title",
+            "description",
+            "ingredients",
+            "instructions",
+            "meal_type",
+            "cuisine_types",
+        ]
+
+        ingredients = forms.CharField(widget=RichTextWidget())
+        instructions = forms.CharField(widget=RichTextWidget())
+
+        widget = {
+            "description": forms.Textarea(attrs={"rows": 5}),
+        }
+
+        labels = {
+            "title": "Recipe Title",
+            "description": "Description",
+            "ingredients": "Recipe Ingredients",
+            "instructions": "Recipe Instructions",
+            "meal_type": "Meal Type",
+            "cuisine_types": "Cuisine Type",
+        }
+
+
